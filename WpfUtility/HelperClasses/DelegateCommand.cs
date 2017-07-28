1a1080723b18e4d91ce7fc729d6bbee19006e0d9
Eike Stremlau <eike.stremlau@gmail.com>
2017-07-28 23:34:32 +0200

    Added first version of loading animation

--- WpfUtility/HelperClasses/DelegateCommand.cs
+++ WpfUtility/HelperClasses/DelegateCommand.cs
@@ -1,15 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Input;
 
 namespace WpfUtility
 {
+    /// <summary>
+    /// This class allows delegating the commanding logic to methods passed as parameters,
+    /// and enables a View to bind commands to objects that are not part of the element tree.
+    /// </summary>
     public class DelegateCommand : ICommand
     {
         private readonly Action _action;
+        private readonly Func<bool> _canExecute;
+        private List<WeakReference> _canExecuteChangedHandlers;
 
-        public DelegateCommand(Action action)
+        //public DelegateCommand(Action action)
+        //{
+        //    _action = action;
+        //}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// </summary>
+        /// <param name="executeMethod">The execute method.</param>
+        public DelegateCommand(Action executeMethod) : this(executeMethod, null)
         {
-            _action = action;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// </summary>
+        /// <param name="executeMethod">The execute method.</param>
+        /// <param name="canExecuteMethod">The can execute method.</param>
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            if (executeMethod == null)
+            {
+                throw new ArgumentNullException(nameof(executeMethod));
+            }
+
+            _action = executeMethod;
+            _canExecute = canExecuteMethod;
         }
 
         public void Execute(object parameter)
@@ -17,11 +47,71 @@ namespace WpfUtility
             _action();
         }
 
-        public bool CanExecute(object parameter)
+        /// <summary>
+        /// Method to determine if the command can be executed.
+        /// </summary>
+        /// <returns><c>true</c> if this instance can execute; otherwise, <c>false</c>.</returns>
+        public bool CanExecute()
         {
-            return true;
+            return _canExecute == null || _canExecute();
         }
 
-        public event EventHandler CanExecuteChanged;
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManagerHelper.AddWeakReferenceHandler(ref _canExecuteChangedHandlers, value);
+            }
+            remove
+            {
+                CommandManagerHelper.RemoveWeakReferenceHandler(_canExecuteChangedHandlers, value);
+            }
+        }
+
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns><c>true</c> if this command can be executed; otherwise, <c>false</c>.</returns>
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute();
+        }
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        void ICommand.Execute(object parameter)
+        {
+            Execute();
+        }
+
+        /// <summary>
+        /// Executes this instance.
+        /// </summary>
+        public void Execute()
+        {
+            _action?.Invoke();
+        }
+
+        /// <summary>
+        /// Raises the CanExecuteChaged event
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            OnCanExecuteChanged();
+        }
+
+        /// <summary>
+        /// Protected virtual method to raise CanExecuteChanged event
+        /// </summary>
+        protected virtual void OnCanExecuteChanged()
+        {
+            CommandManagerHelper.CallWeakReferenceHandlers(_canExecuteChangedHandlers);
+        }
     }
 }
